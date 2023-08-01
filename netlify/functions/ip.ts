def38563e6ae26e386dd8f7c24a838f6b0438076
Sil van Diepen <me@sil.mt>
2023-08-01 15:23:06 +0400

    feature: add ip address to response

--- netlify/functions/ip.ts
+++ netlify/functions/ip.ts
@@ -1,9 +1,15 @@
+var ip = require("ip");
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+
+
+const getIpAddress = ()=>{
+return ip.address();
+}
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: "Hello World", ip: getIpAddress() }),
   };
 };
 
